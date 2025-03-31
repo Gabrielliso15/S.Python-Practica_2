1569665f85f01f35cf322a4b178a2b81a598bdc2
Gabriel Nicolas Liso <gabrielliso@yahoo.com.ar>
2025-03-31 20:22:39 -0300

    Commit ejercicio 3

--- src/functions.py
+++ src/functions.py
@@ -5,3 +5,10 @@ def Cumple(linea):
     if len(palabras) > 1 and palabras[1][0].lower() in "aeiou":
         return True
     return False
+
+# Ejercicio 3
+
+def Cumple3 (linea,clave):
+    if clave in linea:
+        return True
+    return False
